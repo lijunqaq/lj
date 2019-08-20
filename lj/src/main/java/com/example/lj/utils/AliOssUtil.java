2a2535953f581f483b338d9aca6f154b03ef9d0f
lijun <lijun@yhbj.cn>
2019-08-20 10:17:59 +0800

    11

---
+++ lj/src/main/java/com/example/lj/utils/AliOssUtil.java
@@ -0,0 +1,35 @@
+package com.example.lj.utils;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+
+import java.io.ByteArrayInputStream;
+
+/**
+ * 功能描述
+ *
+ * @author Lj
+ * @date 2019/8/16
+ */
+public class AliOssUtil {
+
+    // Endpoint以杭州为例，其它Region请按实际情况填写。
+    String endpoint = "http://oss-cn-shenzhen.aliyuncs.com";
+    // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+    String accessKeyId = "LTAIyCTTGgCKmb09";
+    String accessKeySecret = "YFBD6QUb7Q4MJe1CrcUmcAxiDQxM7l";
+    String bucketName = "lijunqaq";
+    String objectName = "img/";
+
+
+    public void upload(String context) {
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        // 上传内容到指定的存储空间（bucketName）并保存为指定的文件名称（objectName）。
+        ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(context.getBytes()));
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+    }
+}
